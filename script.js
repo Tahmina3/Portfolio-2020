7bb0da2183c4d90a610391b47caac7df9f5c8e61
Tahmina Rahman <tahminar66@gmail.com>
2020-03-03 11:03:39 +0000

    contact form

--- script.js
+++ script.js
@@ -2,7 +2,37 @@ function openNavbar() {
     document.querySelector("#navbar").style.width = "100%";
     document.querySelectorAll(".open")[0].style.opacity = 0;
   }
+
   function closeNavbar() {
     document.querySelector("#navbar").style.width = "0";
     document.querySelectorAll(".open")[0].style.opacity = 1;
   }
+
+
+  //https://www.tutorialspoint.com/javascript/javascript_form_validations.htm
+  function validateForm() {
+if( document.contactForm.Name.value == "" ) {
+            alert( "Please provide your name!" );
+            document.contactForm.Name.focus() ;
+            return false;
+         }
+         if( document.contactForm.EMail.value == "" ) {
+            alert( "Please provide your Email!" );
+            document.contactForm.EMail.focus() ;
+            return false;
+         }
+         return( true );
+  }
+
+  function validateEmail() {
+    var emailID = document.contactForm.EMail.value;
+    atpos = emailID.indexOf("@");
+    dotpos = emailID.lastIndexOf(".");
+    
+    if (atpos < 1 || ( dotpos - atpos < 2 )) {
+       alert("Please enter correct email ID")
+       document.contactForm.EMail.focus() ;
+       return false;
+    }
+    return( true );
+ }
